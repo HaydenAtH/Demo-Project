8515bc248956cd99e8c499bd6df2113e564b57b0
Percival <114445268+HaydenAtH@users.noreply.github.com>
2024-04-19 09:34:19 -0500

    first commit

---
+++ MyClass.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class MyClass {
+    public static void main(String[] args){
+        int a = 4;
+
+        System.out.println(a);
+
+        Scanner scnr = new Scanner(System.in);
+    }
+}
